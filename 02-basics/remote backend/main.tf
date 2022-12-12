bec28556c85bb4c72ff0e858bac436532e4e4d02
longy <longhanzhang999@gmail.com>
2022-12-12 22:02:08 +1030

    Create S3 bucket

--- 02-basics/remote backend/main.tf
+++ 02-basics/remote backend/main.tf
@@ -1,27 +1,14 @@
-terraform {
-  backend "s3" {
-    bucket         = "longys3"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-locking"
-    encrypt        = true
-    key            = "02-basics/remote backend/main.tf"
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
+  region = var.aws_region
 }
 
-resource "aws_s3_instance" "example" {
-  # bucket        = "longys3"
-  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
-  instance_type = "t2.micro"
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket_prefix = var.bucket_prefix
+  acl           = var.acl
+
+  versioning {
+    enabled = var.versioning
+  }
+
+  tags = var.tags
 }
